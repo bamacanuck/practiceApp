6e117a62ec1c344d088dfd5097240e806b0a2e22
Shane Scott <shaneradio@gmail.com>
2018-10-03 16:04:47 -0400

    exercise - second server

--- jserver.js
+++ jserver.js
@@ -1,7 +1,9 @@
 // VITAL STEP ONE : load necessary (http) module/modules
 let http = require ('http');
 
-const PORT = 8080;
+const PORTOne = 8080;
+
+const PORTTwo = 9999;
 
 // VITAL STEP FOUR (in part): set the handler/handlers
 function handleRequest (req, res) {
@@ -10,9 +12,15 @@ function handleRequest (req, res) {
 
 // VITAL STEP TWO : declare the server
 // VITAL STEP FOUR (in part): set the handler/handlers
-const server = http.createServer(handleRequest);
+const serverOne = http.createServer(handleRequest);
+
+const serverTwo = http.createServer(handleRequest);
 
 // VITAL STEP THREE: set the listener
-server.listen (PORT, function () {
-    console.log("server now listening on port: " + PORT);
+serverOne.listen (PORTOne, function () {
+    console.log("server now listening on port: " + PORTOne);
+})
+
+serverTwo.listen (PORTTwo, function () {
+    console.log("server now listening on port: " + PORTTwo);
 })
