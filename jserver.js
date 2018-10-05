a2891c575982eb26258a1c7a122fc16c63e2ed42
S.R. Scott <shaneradio@gmail.com>
2018-10-05 13:53:07 -0400

    commented out second-server exercise lines

--- jserver.js
+++ jserver.js
@@ -1,31 +1,31 @@
 // VITAL STEP ONE : load necessary (http) module/modules
 let http = require ('http');
 
-const PORTOne = 8080;
+const PORT = 8080;
 
-const PORTTwo = 9999;
+// const PORTTwo = 9999;
 
 // VITAL STEP FOUR (in part): set the handler/handlers
 function handleRequest (req, res) {
     res.end("Here's your response. It's working. " + req.url);
 }
 
-function handleRequestX (req, res) {
-    res.end("Here's your response. It's working on this port, too. " + req.url);
-    process.exit(0);
-}
+// function handleRequestX (req, res) {
+//     res.end("Here's your response. It's working on this port, too. " + req.url);
+//     // process.exit(0);
+// }
 
 // VITAL STEP TWO : declare the server
 // VITAL STEP FOUR (in part): set the handler/handlers
-const serverOne = http.createServer(handleRequest);
+const server = http.createServer(handleRequest);
 
-const serverTwo = http.createServer(handleRequestX);
+// const serverTwo = http.createServer(handleRequestX);
 
 // VITAL STEP THREE: set the listener
-serverOne.listen (PORTOne, function () {
-    console.log("server now listening on port: " + PORTOne);
+server.listen (PORT, function () {
+    console.log("server now listening on port: " + PORT);
 })
 
-serverTwo.listen (PORTTwo, function () {
-    console.log("server now listening on port: " + PORTTwo);
-})
+// serverTwo.listen (PORTTwo, function () {
+//     console.log("server now listening on port: " + PORTTwo);
+// })
