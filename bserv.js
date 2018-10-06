5056d36489e55c64febd889cd034ef5d66474e40
S.R. Scott <shaneradio@gmail.com>
2018-10-05 20:17:27 -0400

    another one... yes, another one... finished

--- bserv.js
+++ bserv.js
@@ -5,5 +5,10 @@ const PORT = 8085;
 let server = http.createServer(handleRequest);
 
 server.listen(PORT, function () {
-    
-})
+    console.log("server listening - port " + PORT);
+});
+
+function handleRequest (req, res) {
+    let path = req.url;
+    res.end("this is the " + path + " path, for routing purposes.");
+};
