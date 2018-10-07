d1e30b073fef3451764163bf0ea1a5b930a35213
Shane Scott <shaneradio@gmail.com>
2018-10-07 14:38:54 -0400

    another

---
+++ zaserver.js
@@ -0,0 +1,28 @@
+const http = require ("http");
+
+let PORT = 8100;
+
+let server = http.createServer(handleReq);
+
+server.listen(PORT, function () {
+    console.log("success... server now listening - port " + PORT);
+});
+
+function handleReq (req, res) {
+    let path = req.url;
+    
+    switch (path) {
+
+        case "/":
+            res.end(" ");
+
+        case "/1":
+            res.end("one one one one one");
+
+        case "/2":
+            res.end("two two two two two");
+
+        default:
+            res.end("nope nope nope nope nope");
+    }
+};
