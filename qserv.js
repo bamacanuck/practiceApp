1e54c1b1cb73bcf650b725a9bf63f214c6a0ee57
S.R. Scott <shaneradio@gmail.com>
2018-10-06 20:30:12 -0400

    a bit more practice/repetition... proved successful

--- qserv.js
+++ qserv.js
@@ -1,2 +1,14 @@
 const http = require("http");
 
+const PORT = 8086;
+
+const server = http.createServer(handlerX);
+
+server.listen(PORT, function () {
+    console.log("huzzah - our server's now listening on port: " + PORT);
+});
+
+function handlerX (req, res) {
+    let path = req.url;
+    res.end("The handlerX function has been called, successfully. \n\nThis is the " + path + " path.");
+}
