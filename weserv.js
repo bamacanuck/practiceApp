6dfeda1a91218f235eb978e3dcabe2f0e164d15a
S.R. Scott <shaneradio@gmail.com>
2018-10-11 11:55:19 -0400

    added default routing instruction

--- weserv.js
+++ weserv.js
@@ -18,8 +18,14 @@ function reqHandler(req, res) {
 
         case "/":
             return fs.readFile(__dirname + "/home1.html", function (err, data) {
-                res.writeHead(200, { "Content-Type": "text-html"});
+                // res.writeHead(200, { "Content-Type": "text-html"});
                 res.end(data);
             });
+
+        // default:
+        //     return fs.readFile(__dirname + "/home1.html", function (err, data) {
+        //         // res.writeHead(200, { "Content-Type": "text-html"});
+        //         res.end(data);
+        //     });
     }
 };
