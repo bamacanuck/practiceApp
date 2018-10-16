e654548fceb028b167e0ccb666d6032b0cf372d7
Shane Scott <shaneradio@gmail.com>
2018-10-16 09:36:42 -0400

    option ordering experiment

--- postIt/server.js
+++ postIt/server.js
@@ -10,10 +10,12 @@ function handleRequest(req, res) {
   var path = req.url;
 
   switch (path) {
-    case "/thanks":
-      return renderThankYouPage(req, res);
+    // case "/thanks":
+      // return renderThankYouPage(req, res);
     default:
       return renderWelcomePage(req, res);
+    case "/thanks":
+      return renderThankYouPage(req, res);
   }
 }
 
