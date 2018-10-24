fe34cbc79ba4488bbcc1624648701930234ec3f0
Shane Scott <shaneradio@gmail.com>
2018-10-24 11:51:40 -0400

    yes... yet closer still to  xserver.js and its accompanying x-name-appended pages replacing sw-ext files

--- SW-ext/xserver.js
+++ SW-ext/xserver.js
@@ -61,6 +61,25 @@ app.get("/allUnits", function (req, res) {
     res.json(units);
 });
 
+// app.post("/api/new", function (req, res) {
+//     res.json(units);
+// });
+
+app.post("/api/new", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newcharacter = req.body;
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newcharacter);
+  
+    characters.push(newcharacter);
+  
+    res.json(newcharacter);
+  });
+
 app.get("/api/:unit?", function (req, res) {
     // note we've used "unit" - not "units" -
     // on the previous and next lines...
